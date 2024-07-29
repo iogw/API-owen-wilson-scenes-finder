ac0984aea87b05ed996cc94c2839706e13d5172a
Irene García <irenegwodak@gmail.com>
2024-07-29 15:29:12 +0200

    change vite config to local/github pages config base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/API-owen-wilson-scenes-finder/"
-})
+export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [react()],
+  };
+
+  if (mode === 'development') {
+    config.base = '/';
+  } else {
+    config.base = '/API-owen-wilson-scenes-finder/';
+  }
+
+  return config;
+});
