1473224ebe59d050d2852831b0738bfc8f067a92
Irene García <irenegwodak@gmail.com>
2024-07-29 15:54:01 +0200

    return to HashRouter

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const config = {
-    plugins: [react()],
-  };
-
-  if (mode === 'development') {
-    config.base = '/';
-  } else {
-    config.base = '/API-owen-wilson-scenes-finder/';
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [react()],
+  base: '/API-owen-wilson-scenes-finder/',
 });
